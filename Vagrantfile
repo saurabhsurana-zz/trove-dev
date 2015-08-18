c6cb5bc60646611ece79baa0d4ce8c2cc6aaa9f5
Saurabh Surana <saurabh.surana@hp.com>
2015-08-18 16:14:49 -0700

    Added - Vagrantfile to provision vagrant vms - nova-boot and cloudinit script to provision the cloud vms

---
+++ Vagrantfile
@@ -0,0 +1,54 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+RAM = 12240
+CPU = 4
+
+OPENSTACK_SOURCE_DIR = ENV['OPENSTACK_SOURCE_DIR'] || raise('Please set env variable "OPENSTACK_SOURCE_DIR')
+OPENSTACK_SYNC_DIR = ENV['OPENSTACK_SYNC_DIR'] || "/home/vagrant/shared-stack"
+ENABLE_NEUTRON = ENV['ENABLE_NEUTRON'] || true
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "juice/box-apple-trusty-xl"
+
+  if OPENSTACK_SOURCE_DIR
+      config.vm.synced_folder OPENSTACK_SOURCE_DIR, OPENSTACK_SYNC_DIR, 
+          create: true, type: "rsync",
+          owner: "vagrant", group: "vagrant",
+	  rsync__auto: true,
+          rsync__args: ["--verbose", "--archive", "-z", "--copy-links"]
+  end
+
+  # ########################################
+  # other sync directories/provisioning scripts
+
+  # ########################################
+
+  config.vm.provision "check_source", type: "shell", run: "always" do |s|
+      s.path = "common/check_source.bash"
+      s.args = [OPENSTACK_SYNC_DIR]
+  end
+
+  config.vm.provision "bootstrap_redstack", type: "shell", run: "always" do |s|
+      s.path = "bootstrap/bootstrap_redstack.bash"
+      s.args = [ENABLE_NEUTRON]
+  end
+
+  config.vm.network "forwarded_port", guest: 80, host: 4681, protocol: 'tcp'
+
+  # ########################################
+  # add other ports
+
+  # ########################################
+
+  config.vm.provider "vmware_fusion" do |v|
+    v.vmx["memsize"] = RAM
+    v.vmx["numvcpus"] = CPU
+    v.vmx["vhv.enable"] = TRUE
+    v.gui = true
+  end
+
+end
---
+++ nova-boot.bash
@@ -0,0 +1,68 @@
+#!/bin/bash -x
+
+ENABLE_NEUTRON=${ENABLE_NEUTRON:-true}
+SEC_GROUP=${SEC_GROUP:-devstack}
+IMAGE_NAME=${IMAGE_NAME:-Ubuntu Server 14.0}
+KEY_NAME=${KEY_NAME:-bigdata-team}
+NETWORK_NAME=${NETWORK_NAME:-devstack}
+FLAVOR=${FLAVOR:-105}
+AZ=${AZ:-az3}
+
+TAG=`date +"%m%d%H%M"`
+if [ ${ENABLE_NEUTRON} == true ]
+then
+    AUTO_INSTANCE_NAME=bd-dev-neutron-net-${TAG}
+else
+    AUTO_INSTANCE_NAME=bd-dev-nova-net-${TAG}
+fi
+
+INSTANCE_NAME=${1:-$AUTO_INSTANCE_NAME}
+
+
+nova secgroup-list | grep ${SEC_GROUP}
+if [ $? -ne 0 ]; then
+    echo "ERROR: Security group ${SEC_GROUP} not found. Set SEC_GROUP to use different security group"
+    exit 1
+fi
+
+nova keypair-list | grep ${KEY_NAME}
+if [ $? -ne 0 ]; then
+    echo "ERROR: keypair ${KEY_NAME} not found. Set KEY_NAME to use different keypair"
+    exit 1
+fi
+
+IMAGE_ID=`nova image-list | grep "${IMAGE_NAME}" | grep -v deprecated | awk '{print $2}'`
+if [ $? -ne 0 ]; then
+    echo "ERROR: Image ${IMAGE_NAME} not found. Set IMAGE_NAME to use different image"
+    exit 1
+fi
+
+NET_ID=`neutron net-list | grep ${NETWORK_NAME} | awk '{print $2}'`
+if [ $? -ne 0 ]; then
+    echo "ERROR: Neutron network ${NET_ID} not found. Set NET_ID to use different neutron network"
+    exit 1
+fi
+
+if [ -f cloudinit-redstack ]
+then
+    sed -i .bash "s/ENABLE_NEUTRON_VALUE/$ENABLE_NEUTRON/g" cloudinit-redstack
+fi
+
+nova boot --image ${IMAGE_ID} --flavor ${FLAVOR} --security_group ${SEC_GROUP} --key_name ${KEY_NAME} --nic net-id=${NET_ID} --availability_zone ${AZ} --user_data cloudinit-redstack.bash ${INSTANCE_NAME}
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed to create nova instance"
+    exit 1
+else
+    INSTANCE_ID=`nova show ${INSTANCE_NAME} | grep id | grep -v user_id | grep -v tenant_id | awk '{print $4}'`
+    FLOATING_IP=`nova floating-ip-list | grep Ext-Net | grep None | head -n 1 | awk '{print $2}'`
+    if [ $? -ne 0 ] || [ -z ${FLOATING_IP} ]; then
+        FLOATING_IP=`nova floating-ip-create | grep Ext-Net | grep None | head -n 1 | awk '{print $2}'`
+        if [ $? -ne 0 ]; then
+            echo "ERROR: Failed to associate floating ip with the Instance ${INSTANCE_ID}"
+            exit 1
+        fi
+    fi
+    echo "Waiting for floating ip"
+    sleep 20
+    nova add-floating-ip ${INSTANCE_ID} ${FLOATING_IP}
+fi
